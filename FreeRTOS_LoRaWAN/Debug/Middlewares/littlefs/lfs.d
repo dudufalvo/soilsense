62dce1683b8b0aad82e89ba82c30f6a93c1bb20f
Eduardo Falvo <dudu.falvo@gmail.com>
2024-06-03 22:50:50 +0100

    build stp

--- FreeRTOS_LoRaWAN/Debug/Middlewares/littlefs/lfs.d
+++
@@ -1,6 +0,0 @@
-Middlewares/littlefs/lfs.o: \
- /Users/gabrielsilva/UC/3Ano/2\ Semestre/Projeto\ 2/LoRaWan-E5-Node-main/Middlewares/Third_Party/littlefs/lfs.c \
- /Users/gabrielsilva/UC/3Ano/2\ Semestre/Projeto\ 2/LoRaWan-E5-Node-main/Middlewares/Third_Party/littlefs/lfs.h \
- /Users/gabrielsilva/UC/3Ano/2\ Semestre/Projeto\ 2/LoRaWan-E5-Node-main/Middlewares/Third_Party/littlefs/lfs_util.h
-/Users/gabrielsilva/UC/3Ano/2\ Semestre/Projeto\ 2/LoRaWan-E5-Node-main/Middlewares/Third_Party/littlefs/lfs.h:
-/Users/gabrielsilva/UC/3Ano/2\ Semestre/Projeto\ 2/LoRaWan-E5-Node-main/Middlewares/Third_Party/littlefs/lfs_util.h:
--- frontend/src/schemas/recoverPassword.tsx
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup'
-
-export const recoverPasswordSchema = yup.object().shape({
-  email: yup.string()
-    .required('Email is required')
-    .email('Email is invalid')
-})
